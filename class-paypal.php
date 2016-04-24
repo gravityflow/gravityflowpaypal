8f30c360ecf636eb7255afe2738cd79efcec6163
Steve Henty <stevehenty@gmail.com>
2016-04-24 09:48:47 +0200

    first commit

---
+++ class-paypal.php
@@ -0,0 +1,177 @@
+<?php
+
+
+// Make sure Gravity Forms is active and already loaded.
+if ( class_exists( 'GFForms' ) ) {
+
+	class Gravity_Flow_PayPal extends Gravity_Flow_Extension {
+
+		private static $_instance = null;
+
+		public $_version = GRAVITY_FLOW_PAYPAL_VERSION;
+
+		public $edd_item_name = GRAVITY_FLOW_PAYPAL_EDD_ITEM_NAME;
+
+		// The Framework will display an appropriate message on the plugins page if necessary
+		protected $_min_gravityforms_version = '1.9.10';
+
+		protected $_slug = 'gravityflowpaypal';
+
+		protected $_path = 'gravityflowpaypal/paypal.php';
+
+		protected $_full_path = __FILE__;
+
+		// Title of the plugin to be used on the settings page, form settings and plugins page.
+		protected $_title = 'PayPal Extension';
+
+		// Short version of the plugin title to be used on menus and other places where a less verbose string is useful.
+		protected $_short_title = 'PayPal';
+
+		protected $_capabilities = array(
+			'gravityflowpaypal_uninstall',
+			'gravityflowpaypal_settings',
+		);
+
+		protected $_capabilities_app_settings = 'gravityflowpaypal_settings';
+		protected $_capabilities_uninstall = 'gravityflowpaypal_uninstall';
+
+		public static function get_instance() {
+			if ( self::$_instance == null ) {
+				self::$_instance = new Gravity_Flow_PayPal();
+			}
+
+			return self::$_instance;
+		}
+
+		private function __clone() {
+		} /* do nothing */
+
+
+		public function pre_init() {
+			parent::pre_init();
+			add_action( 'gform_paypal_fulfillment', array(
+				$this,
+				'action_gform_paypal_fulfillment'
+			), 10, 4 );
+			add_action( 'wp', array( $this, 'maybe_redirect' ), 9 );
+			add_action( 'admin_init', array( $this, 'maybe_redirect' ), 9 );
+		}
+
+		public function init() {
+			parent::init();
+			add_filter( 'gravityflow_permission_denied_message_entry_detail', array( $this, 'filter_gravityflow_permission_denied_message_entry_detail' ), 10, 2 );
+		}
+
+		public function action_gform_paypal_fulfillment( $entry, $feed, $transaction_id, $amount ) {
+			$this->log_debug( __METHOD__ . '() - starting' );
+			$api = new Gravity_Flow_API( $entry['form_id'] );
+			$current_step = $api->get_current_step( $entry );
+			if ( $current_step && $current_step->get_type() == 'paypal' ) {
+
+				$custom = $_REQUEST['custom'];
+				list( $entry_id, $entry_id_hash, $assignee_key_b64, $assignee_key_hash ) = explode( '|', $custom );
+				$assignee_key = base64_decode( $assignee_key_b64 );
+				$assignee_key_hash_check = substr( wp_hash( 'gflow' . $assignee_key ), 0, 4 );
+
+				if ( empty( $assignee_key ) || empty( $assignee_key_hash ) || $assignee_key_hash !== $assignee_key_hash_check ) {
+					$this->log_debug( __METHOD__ . '() - invalid assignee' );
+					return;
+				}
+
+				$assignee = new Gravity_Flow_Assignee( $assignee_key, $current_step );
+
+				$assignee->update_status( 'complete' );
+
+				$current_step->update_step_status( 'complete' );
+
+				$user_id = ( $assignee->get_type() == 'user_id' ) ? $assignee->get_id() : 0;
+
+				$note = sprintf( esc_html__( 'Payment has been completed. Transaction ID: %s', 'gravityflowpaypal' ), $transaction_id );
+
+				$current_step->add_note( $note, $user_id, $assignee->get_display_name() );
+
+				$api->process_workflow( $entry['id'] );
+			}
+		}
+
+		function maybe_redirect() {
+
+			if ( ! isset( $_REQUEST['workflow_paypal_assignee_key'] ) ) {
+				return;
+			}
+
+			/* @var GFPayPal $add_on */
+			$add_on = gf_paypal();
+
+			$entry_id = absint( rgget( 'lid' ) );
+			$entry = GFAPI::get_entry( $entry_id );
+			$form = GFAPI::get_form( $entry['form_id'] );
+
+			// todo: log note
+			// todo: log event
+
+			$validation_result = array( 'is_valid' => true, 'form' => $form, 'failed_validation_page' => 0 );
+			$add_on->validation( $validation_result );
+			$form_id = absint( $form['id'] );
+			$feed = $add_on->get_paypal_feed( $form_id, $entry );
+			$submission_data = $add_on->get_submission_data( $feed, $form, $entry );
+
+			add_filter( 'gform_paypal_return_url', array( $this, 'filter_gform_paypal_return_url' ), 10, 4 );
+
+			$url = $add_on->redirect_url( $feed, $submission_data, $form, $entry );
+
+			$assignee_key = sanitize_text_field( $_REQUEST['workflow_paypal_assignee_key'] );
+
+			// Add Gravity Flow custom vars to the IPN custom query var
+
+			parse_str( $url, $vars ); // Extract the 'custom' var
+			$custom = $vars['custom'];
+			$custom .= '|' . base64_encode( $assignee_key );
+			$custom .= '|' . substr( wp_hash( 'gflow' . $assignee_key ), 0, 4 );
+			$url = add_query_arg( array( 'custom' => $custom ), $url );
+			header( "Location: {$url}" );
+		}
+
+		public function filter_gravityflow_permission_denied_message_entry_detail( $message, $current_step ) {
+			$instance = gf_paypal();
+
+			if ( ! $instance->is_gravityforms_supported() ) {
+				return $message;
+			}
+
+			if ( $str = rgget( 'gf_paypal_return' ) ) {
+				$str = base64_decode( $str );
+
+				parse_str( $str, $query );
+				if ( wp_hash( 'ids=' . $query['ids'] ) == $query['hash'] ) {
+					list( $form_id, $entry_id ) = explode( '|', $query['ids'] );
+					$entry = GFAPI::get_entry( $entry_id );
+					$step_id = rgget( 'workflow_step_id' );
+					$step_id_hash = rgget( 'workflow_step_id_hash' );
+					$step_id_hash_check = wp_hash( 'gflow' . $step_id );
+					if ( $step_id_hash === $step_id_hash_check ) {
+						$api = new Gravity_Flow_Api( $form_id );
+						$step = $api->get_step( $step_id, $entry );
+						if ( $step ) {
+							$message = $step->confirmation_message;
+						}
+					}
+				}
+			}
+			return $message;
+		}
+
+		public function filter_gform_paypal_return_url( $url, $form_id, $lead_id, $query ) {
+			$step_id = absint( $_REQUEST['workflow_paypal_step_id'] );
+
+			if ( empty( $step_id ) ) {
+				return $url;
+			}
+
+			$url = add_query_arg( array( 'workflow_step_id' => $step_id ), $url );
+			$url = add_query_arg( array( 'workflow_step_id_hash' => wp_hash( 'gflow' . $step_id ) ), $url );
+
+			return $url;
+		}
+	}
+}
---
+++ includes/class-step-paypal.php
@@ -0,0 +1,430 @@
+<?php
+/**
+ * Gravity Flow PayPal
+ *
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Steps
+ * @copyright   Copyright (c) 2015, Steven Henty
+ * @license     http://opensource.org/licenses/gpl-3.0.php GNU Public License
+ * @since       1.0
+ */
+if ( class_exists( 'Gravity_Flow_Step' ) ) {
+
+	class Gravity_Flow_Step_Feed_Paypal extends Gravity_Flow_Step_Feed_Add_On{
+		public $_step_type = 'paypal';
+
+		protected $_class_name = 'GFPayPal';
+
+		public function get_label() {
+			return esc_html__( 'PayPal', 'gravityflow' );
+		}
+
+		public function get_icon_url() {
+			return gravity_flow_paypal()->get_base_url() . '/images/paypal.svg';
+		}
+
+		function intercept_submission() {
+			$add_on = call_user_func( array( $this->get_feed_add_on_class_name(), 'get_instance' ) );
+			remove_filter( 'gform_entry_post_save', array( $add_on, 'entry_post_save' ), 10 );
+		}
+
+		function get_settings() {
+
+			$settings = parent::get_settings();
+
+			$account_choices = gravity_flow()->get_users_as_choices();
+
+			$paypal_settings = array(
+				array(
+					'name'       => 'type',
+					'label'      => __( 'Assign To:', 'gravityflowpaypal' ),
+					'type'       => 'radio',
+					'default_value' => 'select',
+					'horizontal' => true,
+					'choices'    => array(
+						array( 'label' => __( 'Select Users', 'gravityflowpaypal' ), 'value' => 'select' ),
+						array( 'label' => __( 'Conditional Routing', 'gravityflowpaypal' ), 'value' => 'routing' ),
+					),
+				),
+				array(
+					'id'       => 'assignees',
+					'name'     => 'assignees[]',
+					'multiple' => 'multiple',
+					'label'    => 'Select Assignees',
+					'type'     => 'select',
+					'choices'  => $account_choices,
+				),
+				array(
+					'name'  => 'routing',
+					'tooltip'   => __( 'Build assignee routing rules by adding conditions. Users and roles fields will appear in the first drop-down field. If the form contains any assignee fields they will also appear here. Select the assignee and define the condition for that assignee. Add as many routing rules as you need.', 'gravityflow' ),
+					'label' => __( 'Routing', 'gravityflowpaypal' ),
+					'type'  => 'routing',
+				),
+				array(
+					'name'  => 'instructions',
+					'label' => __( 'Instructions', 'gravityflowpaypal' ),
+					'type'  => 'checkbox_and_textarea',
+					'tooltip' => esc_html__( 'Activate this setting to display instructions to the user for the current step.', 'gravityflowpaypal' ),
+					'checkbox' => array(
+						'label' => esc_html__( 'Display instructions', 'gravityflowpaypal' ),
+					),
+					'textarea'  => array(
+						'use_editor' => true,
+					),
+				),
+				array(
+					'name'     => 'display_fields',
+					'label'    => __( 'Display Fields', 'gravityflowpaypal' ),
+					'tooltip'   => __( 'Select the fields to hide or display.', 'gravityflowpaypal' ),
+					'type'     => 'display_fields',
+				),
+				array(
+					'name'    => 'assignee_notification_enabled',
+					'label'   => 'Assignee Email',
+					'type'    => 'checkbox',
+					'choices' => array(
+						array(
+							'label'         => __( 'Enabled' ),
+							'name'          => 'assignee_notification_enabled',
+							'default_value' => 1,
+						),
+					),
+				),
+				array(
+					'name'  => 'assignee_notification_from_name',
+					'label' => __( 'From Name', 'gravityflowpaypal' ),
+					'class' => 'fieldwidth-2 merge-tag-support mt-hide_all_fields mt-position-right ui-autocomplete-input',
+					'type'  => 'text',
+				),
+				array(
+					'name'  => 'assignee_notification_from_email',
+					'label' => __( 'From Email', 'gravityflowpaypal' ),
+					'type'  => 'text',
+					'class' => 'fieldwidth-2 merge-tag-support mt-hide_all_fields mt-position-right ui-autocomplete-input',
+					'default_value' => '{admin_email}',
+				),
+				array(
+					'name'  => 'assignee_notification_reply_to',
+					'class' => 'fieldwidth-2 merge-tag-support mt-hide_all_fields mt-position-right ui-autocomplete-input',
+					'label' => __( 'Reply To', 'gravityflowpaypal' ),
+					'type'  => 'text',
+				),
+				array(
+					'name'  => 'assignee_notification_bcc',
+					'class' => 'fieldwidth-2 merge-tag-support mt-hide_all_fields mt-position-right ui-autocomplete-input',
+					'label' => __( 'BCC', 'gravityflowpaypal' ),
+					'type'  => 'text',
+				),
+				array(
+					'name'  => 'assignee_notification_subject',
+					'class' => 'fieldwidth-1 merge-tag-support mt-hide_all_fields mt-position-right ui-autocomplete-input',
+					'label' => __( 'Subject', 'gravityflowpaypal' ),
+					'type'  => 'text',
+				),
+				array(
+					'name'  => 'assignee_notification_message',
+					'label' => 'Message',
+					'type'  => 'visual_editor',
+					'default_value' => '',
+				),
+				array(
+					'name' => 'resend_assignee_email',
+					'label' => __( 'Send reminder', 'gravityflowpaypal' ),
+					'type' => 'checkbox_and_text',
+					'text' => array(
+						'default_value' => 7,
+						'before_input' => __( 'Resend the assignee email after', 'gravityflowpaypal' ),
+						'after_input' => ' ' . __( 'day(s)', 'gravityflowpaypal' ),
+					),
+				),
+				array(
+					'name'  => 'confirmation_message',
+					'label' => esc_html__( 'Confirmation Message', 'gravityflowpaypal' ),
+					'type'  => 'visual_editor',
+					'default_value' => esc_html__( 'Thank you. Your payment is currently being processed', 'gravityflowpaypal' ),
+				),
+			);
+
+			$settings['fields'] = array_merge( $settings['fields'], $paypal_settings );
+
+			return $settings;
+		}
+
+		function process() {
+			$complete = $this->is_complete();
+
+			$assignees = $this->get_assignees();
+
+			if ( empty( $assignees ) ) {
+				$note = sprintf( __( '%s: not required', 'gravityflow' ), $this->get_name() );
+				$this->add_note( $note, 0 , 'gravityflow' );
+			} else {
+				foreach ( $assignees as $assignee ) {
+					$assignee->update_status( 'pending' );
+					// send notification
+					$this->maybe_send_assignee_notification( $assignee );
+					$complete = false;
+				}
+			}
+			return $complete;
+		}
+
+		public function get_status() {
+
+			if ( $this->is_queued() ) {
+				return 'queued';
+			}
+
+			$assignee_details = $this->get_assignees();
+
+			$step_status = 'complete';
+
+			foreach ( $assignee_details as $assignee ) {
+				$user_status = $assignee->get_status();
+
+				if ( empty( $user_status ) || $user_status == 'pending' ) {
+					$step_status = 'pending';
+				}
+			}
+
+			return $step_status;
+		}
+
+		/**
+		 * @deprecated
+		 * @param $form
+		 */
+		public function workflow_detail_status_box( $form ) {
+
+			_deprecated_function( 'workflow_detail_status_box', '1.3.2', 'workflow_detail_box' );
+
+			$default_args = array(
+				'display_empty_fields' => true,
+				'check_permissions' => true,
+				'show_header' => true,
+				'timeline' => true,
+				'display_instructions' => true,
+				'sidebar' => true,
+				'step_status' => true,
+				'workflow_info' => true,
+			);
+
+			$this->workflow_detail_box( $form, $default_args );
+		}
+
+		public function workflow_detail_box( $form, $args ) {
+			global $current_user;
+
+			if ( rgget( 'gf_paypal_return' ) ) {
+				echo $this->confirmation_message;
+				return;
+			}
+
+			$form_id = absint( $form['id'] );
+
+			$status_str            = __( 'Pending Payment', 'gravityflow' );
+			$approve_icon      = '<i class="fa fa-check" style="color:green"></i>';
+			$input_step_status = $this->get_status();
+			if ( $input_step_status == 'complete' ) {
+				$status_str = $approve_icon . __( 'Complete', 'gravityflow' );
+			} elseif ( $input_step_status == 'queued' ) {
+				$status_str = __( 'Queued', 'gravityflow' );
+			}
+
+			$display_step_status = (bool) $args['step_status'];
+			?>
+
+			<div>
+				<?php if ( $display_step_status ) : ?>
+				<h4 style="margin-bottom:10px;"><?php echo $this->get_name() . ' (' . $status_str . ')'?></h4>
+				<ul>
+					<?php
+					$assignees = $this->get_assignees();
+
+					gravity_flow()->log_debug( __METHOD__ . '(): assignee details: ' . print_r( $assignees, true ) );
+
+					foreach ( $assignees as $assignee ) {
+
+						gravity_flow()->log_debug( __METHOD__ . '(): showing status for: ' . $assignee->get_key() );
+
+						$assignee_status = $assignee->get_status();
+
+						gravity_flow()->log_debug( __METHOD__ . '(): assignee status: ' . $assignee_status );
+
+						if ( ! empty( $assignee_status ) ) {
+
+							$assignee_type = $assignee->get_type();
+							$assignee_id = $assignee->get_id();
+
+							if ( $assignee_type == 'user_id' ) {
+								$user_info = get_user_by( 'id', $assignee_id );
+								$status_label = $this->get_status_label( $assignee_status );
+								echo sprintf( '<li>%s: %s (%s)</li>', esc_html__( 'User', 'gravityflow' ), $user_info->display_name,  $status_label );
+							} elseif ( $assignee_type == 'email' ) {
+								$email = $assignee_id;
+								$status_label = $this->get_status_label( $assignee_status );
+								echo sprintf( '<li>%s: %s (%s)</li>', esc_html__( 'Email', 'gravityflow' ), $email,  $status_label );
+
+							} elseif ( $assignee_type == 'role' ) {
+								$status_label = $this->get_status_label( $assignee_status );
+								$role_name = translate_user_role( $assignee_id );
+								echo sprintf( '<li>%s: (%s)</li>', esc_html__( 'Role', 'gravityflow' ), $role_name, $status_label );
+								echo '<li>' . $role_name . ': ' . $assignee_status . '</li>';
+							}
+						}
+					}
+
+					?>
+					</ul>
+					<?php endif; ?>
+				<div>
+					<?php
+
+					if ( $token = gravity_flow()->decode_access_token() ) {
+						$assignee_key = sanitize_text_field( $token['sub'] );
+					} else {
+						$assignee_key = 'user_id|' . $current_user->ID;
+
+					}
+					$assignee = new Gravity_Flow_Assignee( $assignee_key, $this );
+					$assignee_status = $assignee->get_status();
+
+					$role_status = false;
+					foreach ( gravity_flow()->get_user_roles() as $role ) {
+						$role_status = $this->get_role_status( $role );
+						if ( $role_status == 'pending' ) {
+							break;
+						}
+					}
+
+					$paypal_assignee_key = '';
+
+					if ( $assignee_status == 'pending' ) {
+						$paypal_assignee_key = $assignee_key;
+					} elseif ( $role_status == 'pending' ) {
+						$paypal_assignee_key = 'role|' . $role;
+					}
+
+					?>
+				</div>
+				<?php
+
+				$can_update = $assignee_status == 'pending' || $role_status == 'pending';
+
+				if ( $can_update ) {
+					?>
+					<input type="hidden" name="workflow_paypal_assignee_key" value="<?php echo $paypal_assignee_key; ?>" />
+					<input type="hidden" name="workflow_paypal_step_id" value="<?php echo $this->get_id(); ?>" />
+					<br /><br />
+					<div style="text-align:right;">
+						<input class="button button-primary" type="submit" name="gravityflow_paypal_pay" value="<?php esc_html_e( 'Pay', 'gravityflow' ); ?>" />
+					</div>
+					<?php
+				}
+
+				?>
+			</div>
+			<?php
+		}
+
+		public function entry_detail_status_box( $form ) {
+			$status = $this->get_status();
+			?>
+			<h4 style="padding:10px;"><?php echo $this->get_name() . ': ' . $status ?></h4>
+
+			<div style="padding:10px;">
+				<ul>
+					<?php
+
+					$assignees = $this->get_assignees();
+
+					foreach ( $assignees as $assignee ) {
+
+						$assignee_type = $this->get_type();
+
+						$status = $assignee->get_status();
+
+						if ( ! empty( $user_status ) ) {
+							$status_label = $this->get_status_label( $status );
+							switch ( $assignee_type ) {
+								case 'email':
+									echo sprintf( '<li>%s: %s (%s)</li>', esc_html__( 'Email', 'gravityflow' ), $this->get_id(),  $status_label );
+									break;
+								case 'user_id' :
+									$user_info = get_user_by( 'id', $assignee->get_id() );
+									echo '<li>' . esc_html__( 'User', 'gravityflow' ) . ': ' . $user_info->display_name . '<br />' . esc_html__( 'Status', 'gravityflow' ) . ': ' . esc_html( $status_label ) . '</li>';
+									break;
+								case 'role' :
+
+									$role_name = translate_user_role( $assignee->get_id() );
+									echo '<li>' . $role_name . ': ' . esc_html( $status_label ) . '</li>';
+									break;
+							}
+						}
+					}
+
+					?>
+				</ul>
+			</div>
+			<?php
+		}
+
+		public function get_assignees() {
+
+			$assignees = array();
+
+			$assignee_details = array();
+
+			$input_type = $this->type;
+
+			switch ( $input_type ) {
+				case 'select':
+					foreach ( $this->assignees as $assignee_key ) {
+						list( $assignee_type, $assignee_id ) = explode( '|', $assignee_key );
+						$assignee_details[] = new Gravity_Flow_Assignee( array(
+							'id'              => $assignee_id,
+							'type'            => $assignee_type,
+						), $this );
+					}
+					break;
+				case 'routing' :
+					$routings = $this->routing;
+					if ( is_array( $routings ) ) {
+						$entry = $this->get_entry();
+						foreach ( $routings as $routing ) {
+							$assignee_key = rgar( $routing, 'assignee' );
+							if ( in_array( $assignee_key, $assignees ) ) {
+								continue;
+							}
+							list( $assignee_type, $assignee_id ) = explode( '|', $assignee_key );
+							if ( $entry ) {
+								if ( $this->evaluate_routing_rule( $routing ) ) {
+									$assignee_details[] = new Gravity_Flow_Assignee( array(
+										'id'              => $assignee_id,
+										'type'            => $assignee_type,
+									), $this );
+									$assignees[] = $assignee_key;
+								}
+							} else {
+								$assignee_details[] = new Gravity_Flow_Assignee( array(
+									'id'              => $assignee_id,
+									'type'            => $assignee_type,
+								), $this );
+								$assignees[] = $assignee_key;
+							}
+						}
+					}
+
+					break;
+			}
+
+			gravity_flow()->log_debug( __METHOD__ . '(): assignees: ' . print_r( $assignees, true ) );
+
+			return $assignee_details;
+		}
+	}
+}
+
+
+
